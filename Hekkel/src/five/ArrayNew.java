c141c853702b4d76e9f2e15b469e93ba3ddb66ee
Den <denisvinichenko@977.gmail.com>
2024-09-21 09:44:02 +0300

    correction

--- Hekkel/src/five/ArrayNew.java
+++ Hekkel/src/five/ArrayNew.java
@@ -8,9 +8,12 @@ public class ArrayNew {
 	public static void main(String[] args) {
 		int[] a;
 		Random rand = new Random(47);
-		a = new int[rand.nextInt(2)]
+		a = new int[rand.nextInt(2)];
 		System.out.println(a.length);
 		System.out.println(Arrays.toString(a));
+		
+		
+		
 		
 		Integer[] aa = new Integer[rand.nextInt(20)];
 		System.out.println("aa.length = " + aa.length);

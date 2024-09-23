dcbde121828838dcf4f678975d7d938c25ebb4be
Den <denisvinichenko@977.gmail.com>
2024-09-23 20:43:36 +0300

    adding pattern package

---
+++ Hekkel/src/pattern/Command.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class Command {
+
+	public Command() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
